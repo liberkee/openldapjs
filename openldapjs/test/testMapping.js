01bcbf0f277ed3c1fdd90b225d5f87bc319e71e7
MaximRotaru <Maxim.Rotaru@huf-group.com>
2017-04-12 17:08:05 +0300

    Added more tests. There are some tests that do not pass and the code shall be fixed

--- openldapjs/test/testMapping.js
+++ openldapjs/test/testMapping.js
@@ -7,7 +7,15 @@ const JSONmap = require('../modules/mappingJsonObject/mappingStringJson.js');
 
 describe('String to JSON#searchTest', () => {
   const JSONStruct = new JSONmap();
-  let searchResult = '\ndn:ou=users,o=myhost,dc=demoApp,dc=com\nobjectClass:organizationalUnit\nou:users';
+  let simpleLdif = '\ndn:ou=users,o=myhost,dc=demoApp,dc=com\nobjectClass:organizationalUnit\nou:users';
+  let doubleAttrLdif = '\ndn:ou=users,o=myhost,dc=demoApp,dc=com\nobjectClass:organizationalUnit\nobjectClass:organizationalUnit\nou:users';
+  let mixedAttrLdif = '\ndn:ou=users,o=myhost,dc=demoApp,dc=com\nobjectClass:organizationalUnit\nou:users\nobjectClass:userUnit';
+  let mixedDoubledAttrLdif = '\ndn:ou=users,o=myhost,dc=demoApp,dc=com\nobjectClass:organizationalUnit1\nou:users1\nobjectClass:organizationalUnit2\nou:users2';
+  let moreObjLdif = '\ndn:ou=users,o=myhost,dc=demoApp,dc=com\nobjectClass:organizationalUnit1\nou:users1\n\ndn:ou=users,o=myhost,dc=demoApp,dc=com\nobjectClass:organizationalUnit2\nou:users2';
+  let noValueLdif = '\ndn:ou=users,o=myhost,dc=demoApp,dc=com\nobjectClass:organizationalUnit\nou:\nobjectClass:userUnit';
+  let noAttrLdif = '\ndn:ou=users,o=myhost,dc=demoApp,dc=com';
+  let noDnLdif = '\nobjectClass:organizationalUnit\nou:\nobjectClass:userUnit';
+  let emptyDnLdif = '\ndn:\nobjectClass:organizationalUnit\nou:users';
 
   beforeEach((next) => {
     next();
@@ -17,7 +25,7 @@ describe('String to JSON#searchTest', () => {
   });
 
   it('should return the string as JSON', (next) => {
-    JSONStruct.stringLDAPtoJSON(searchResult)
+    JSONStruct.stringLDAPtoJSON(simpleLdif)
     .then((result) => {
       const JSONobjecttest = [{
         dn: 'ou=users,o=myhost,dc=demoApp,dc=com',
@@ -33,6 +41,124 @@ describe('String to JSON#searchTest', () => {
       next();
     });
   });
+
+  it('should return the string as JSON from a ldif with double attribute', (next) => {
+    JSONStruct.stringLDAPtoJSON(doubleAttrLdif)
+    .then((result) => {
+      const JSONobjecttest = [{
+        dn: 'ou=users,o=myhost,dc=demoApp,dc=com',
+        attribute: [
+          {type: 'objectClass', value: ['organizationalUnit', 'organizationalUnit']},
+          {type: 'ou', value: ['users']},
+        ],
+      }];
+      const shouldString = JSON.stringify(JSONobjecttest[0]);
+      const resultString = JSON.stringify(result.entry[0]);
+
+      should.equal(shouldString, resultString);
+      next();
+    });
+  });
+
+
+   it('should return the string as JSON from a ldif with mixed up attribute', (next) => {
+    JSONStruct.stringLDAPtoJSON(mixedAttrLdif)
+    .then((result) => {
+      const JSONobjecttest = [{
+        dn: 'ou=users,o=myhost,dc=demoApp,dc=com',
+        attribute: [
+          {type: 'objectClass', value: ['organizationalUnit', 'userUnit']},
+          {type: 'ou', value: ['users']},
+        ],
+      }];
+      const shouldString = JSON.stringify(JSONobjecttest[0]);
+      const resultString = JSON.stringify(result.entry[0]);
+
+      should.equal(shouldString, resultString);
+      next();
+    });
+  });
+
+
+  it('should return the string as JSON from a ldif with 2 objects', (next) => {
+    JSONStruct.stringLDAPtoJSON(moreObjLdif)
+    .then((result) => {
+      const JSONobjecttest = [{
+        dn: 'ou=users,o=myhost,dc=demoApp,dc=com',
+        attribute: [
+          {type: 'objectClass', value: ['organizationalUnit1']},
+          {type: 'ou', value: ['users1']},
+        ],
+      },
+      {
+        dn: 'ou=users,o=myhost,dc=demoApp,dc=com',
+        attribute: [
+          {type: 'objectClass', value: ['organizationalUnit2']},
+          {type: 'ou', value: ['users2']},
+        ],
+      }];
+      const shouldString0 = JSON.stringify(JSONobjecttest[0]);
+      const shouldString1 = JSON.stringify(JSONobjecttest[1]);
+
+      const resultString0 = JSON.stringify(result.entry[0]);
+      const resultString1 = JSON.stringify(result.entry[1]);
+
+      should.equal(shouldString0, resultString0);
+      should.equal(shouldString1, resultString1);
+      next();
+    });
+  });
+
+  it('should return the string as JSON from a ldif with mixed up doubled attribute', (next) => {
+    JSONStruct.stringLDAPtoJSON(mixedDoubledAttrLdif)
+    .then((result) => {
+      const JSONobjecttest = [{
+        dn: 'ou=users,o=myhost,dc=demoApp,dc=com',
+        attribute: [
+          {type: 'objectClass', value: ['organizationalUnit1', 'organizationalUnit2']},
+          {type: 'ou', value: ['users1', 'users2']},
+        ],
+      }];
+      const shouldString = JSON.stringify(JSONobjecttest[0]);
+      const resultString = JSON.stringify(result.entry[0]);
+
+      should.equal(shouldString, resultString);
+      next();
+    });
+  });
+
+  it('should return an error if an attribute does not have a value', (next) => {
+    JSONStruct.stringLDAPtoJSON(noValueLdif)
+    .catch((err) => {
+      should.deepEqual(err.message, 'The string is not a LDAP structure');
+      next();
+    });
+  });
+
+  it('should return an error if the ldif does not have any attributes', (next) => {
+    JSONStruct.stringLDAPtoJSON(noAttrLdif)
+    .catch((err) => {
+      should.deepEqual(err.message, 'The string is not a LDAP structure');
+      next();
+    });
+  });
+
+  it('should return an error if the dn is missing', (next) => {
+    JSONStruct.stringLDAPtoJSON(noDnLdif)
+    .catch((err) => {
+      should.deepEqual(err.message, 'The string is not a LDAP structure');
+      next();
+    });
+  });
+
+  it('should return an error if the dn has no value', (next) => {
+    JSONStruct.stringLDAPtoJSON(emptyDnLdif)
+    .catch((err) => {
+      should.deepEqual(err.message, 'The string is not a LDAP structure');
+      next();
+    });
+  });
+
 
   it('should return an error if there is a number', (next) => {
     JSONStruct.stringLDAPtoJSON(1234)
