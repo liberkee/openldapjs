9df1f3f2cb9423d432a01db8c171ad0476a38678
Radu94 <radu.aribasoiu@gmail.com>
2017-05-03 10:52:35 +0300

    Began writing test cases for search(empty search)

--- openldapjs/test/testSearch.js
+++ openldapjs/test/testSearch.js
@@ -28,18 +28,18 @@ describe('Testing the async LDAP search ', () => {
   });
 
   it('should return an empty search', (next) => {
-    clientLDAP.search('ou=users,o=myhost,dc=demoApp,dc=com', 2, 'objectclass=person')
+    clientLDAP.search('ou=users,o=myhost,dc=demoApp,dc=com', 2, 'objectclass=')
       .then((result) => {
         console.log(result);
         //should.deepEqual(result,undefined);
         //next();
-        result.should.be.empty;
+       should.deepEqual(result,'');
       }).then(() =>{
         next();
       });
   });
 
-  it('should return a search with non existing searc base', (next) => {
+  it('should return a search with non existing search base', (next) => {
     clientLDAP.search('','base','objectclass=*')
       .then( (result) => {
         console.log('result is : '+ result);
