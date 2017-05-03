a3b1469472c6501324dfe26f307c31e10a5289d6
Radu94 <radu.aribasoiu@gmail.com>
2017-05-03 16:46:55 +0300

    Wrote first search test cases

--- openldapjs/test/testSearch.js
+++ openldapjs/test/testSearch.js
@@ -6,7 +6,9 @@ const LDAPWrap = require('../modules/ldapAsyncWrap.js');
 
 describe('Testing the async LDAP search ', () => {
   const host = 'ldap://localhost:389';
-  const dn = 'cn=admin,dc=demoApp,dc=com';
+  const dnAdmin = 'cn=admin,dc=demoApp,dc=com';
+  const dnUser = 'cn=cghitea,ou=users,o=myhost,dc=demoApp,dc=com';
+
   const password = 'secret';
   let clientLDAP = new LDAPWrap(host);
 
@@ -16,39 +18,84 @@ describe('Testing the async LDAP search ', () => {
 
     clientLDAP.initialize()
       .then(() => {
-        clientLDAP.bind(dn,password)
-        .then(() => {
-          next();
-        });
-        
+        clientLDAP.bind(dnAdmin, password)
+          .then(() => {
+            next();
+          });
+
       });
   });
   afterEach(() => {
-    clientLDAP.unbind();
+    clientLDAP.unbind()
+      .then(() => {
+        next();
+      });
   });
 
   it('should return an empty search', (next) => {
-    clientLDAP.search('ou=users,o=myhost,dc=demoApp,dc=com', 2, 'objectclass=')
+    clientLDAP.search('dc=demoApp,dc=com', 2, 'objectclass=aliens')
       .then((result) => {
         console.log(result);
         //should.deepEqual(result,undefined);
         //next();
-       should.deepEqual(result,'');
-      }).then(() =>{
+        should.deepEqual(result, undefined);
+      }).then(() => {
+        next();
+      });
+  });
+  /**
+   * case for search with non existing search base
+   */
+  it('should return the root node', (next) => {
+    clientLDAP.search('', 0, 'objectclass=*')
+      .then((result) => {
+        //console.log('result is : '+ result);
+        const baseDN = '\ndn:\nobjectClass:top\nobjectClass:OpenLDAProotDSE\n\n';
+        console.log(baseDN);
+        should.deepEqual(result, baseDN);
+
+      }).then(() => {
+        next();
+      });
+
+  });
+  /**
+   * test case for search with access denied
+   */
+
+  it('should return nothing', (next) => {
+    clientLDAP.unbind()
+      .then(() => {
+        clientLDAP.bind(dnUser, password)
+          .then(() => {
+            clientLdap.search('dc=demoApp,cn=com', 2, 'objectClass=*')
+              .then((result) => {
+                console.log('the result is : ' + result);
+                should.deepEqual(result, undefined);
+              });
+          });
+      }).then(() => {
+        next();
+      });
+
+  });
+
+  /**
+   * test case with a single result
+   */
+
+  it('should return a single result', (next) => {
+    clientLDAP.search('dc=demoApp,dc=com', 2, 'objectClass=simpleSecurityObject')
+      .then((result) => {
+        //console.log('single result is: ' +result);
+        const singleResult = '\ndn:cn=admin,dc=demoApp,dc=com\nobjectClass:simpleSecurityObject\nobjectClass:organizationalRole\ncn:admin\ndescription:LDAP administrator\nuserPassword:{SSHA}UU9JBg/X7r6HK/ARkYnmRTLTCNNisZFA\n\n';
+        should.deepEqual(result, singleResult);
+      }).then(() => {
         next();
       });
   });
 
-  it('should return a search with non existing search base', (next) => {
-    clientLDAP.search('','base','objectclass=*')
-      .then( (result) => {
-        console.log('result is : '+ result);
-        should.deepEqual(result,undefined);
-      }).then( () => {
-        next();
-      });
-    
-  });
- 
+
+
 
 });
