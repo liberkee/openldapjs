5479dfd4cede00f93ca4e54b0ebebd4d259a2f58
Radu94 <radu.aribasoiu@gmail.com>
2017-06-20 15:17:21 +0300

    added comments to showcase usage.

--- openldapjs/sample/testLdap.js
+++ openldapjs/sample/testLdap.js
@@ -2,6 +2,11 @@
 
 const LDAPWrap = require('../modules/ldapAsyncWrap.js');
 
+
+/**
+ * an example of program ussage would be node testLdap.js YourHost YourUserDN YourUserPassword Base SearchFIlter                                                                                                 
+ */
+
 const host = process.argv[2];
 const dnUser = process.argv[3];
 const password = process.argv[4];
