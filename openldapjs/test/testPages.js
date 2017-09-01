b8aeb1a51215111b7179e872a6f7746e23b7fc77
radu.aribasoiu <radu.aribasoiu@gmail.com>
2017-09-01 14:34:34 +0200

    testFile added

---
+++ openldapjs/test/testPages.js
@@ -0,0 +1,22 @@
+'use strict'
+const LDAPWrap = require('./modules/ldapAsyncWrap.js');
+const host = 'ldap://localhost:389';
+const dnAdmin = 'cn=admin,dc=demoApp,dc=com';
+const dnUser = 'cn=cghitea,ou=users,o=myhost,dc=demoApp,dc=com';
+const searchBase = 'dc=demoApp,dc=com';
+const password = 'secret';
+let clientLDAP = new LDAPWrap(host);
+
+
+clientLDAP.initialize()
+  .then(() => {
+    clientLDAP.bind(dnAdmin, password)
+      .then(() => {
+        clientLDAP.pagedSearch('dc=demoApp,dc=com', 2, 'objectClass=person', 20)
+          .then((result) => {
+            console.log('result is:'+result);
+          });
+      });
+
+  });
+
