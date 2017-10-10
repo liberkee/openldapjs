48169b4ba6fead9ad3e384daf62c78f3386b0b9b
Radu Aribasoiu <raribas@debian.adhuf>
2017-10-10 17:45:49 +0300

    console.loged errors

--- openldapjs/modules/streamInterface.js
+++ openldapjs/modules/streamInterface.js
@@ -4,7 +4,7 @@ const Readable = require('stream').Readable;
 
 /**
  * @class PagedSearchStream
- * class that extends the readable steram class
+ * class that extends the readable stream class
  */
 class PagedSearchStream extends Readable {
 
@@ -15,10 +15,10 @@ class PagedSearchStream extends Readable {
    * 2(SUBTREE)
    * @param {String} filter search filter.
    * @param {unsigned int} pageSize number of results displayed per page.
-   * @param {unsigned int} searchID unique search id.
+   * @param {unsigned int} searchId unique search id.
    * @param {Object} ldapInstance the ldap instance the search belongs to.
    */
-  constructor(base, scope, filter, pageSize, searchID, ldapInstance) {
+  constructor(base, scope, filter, pageSize, searchId, ldapInstance) {
     super();
     this.objectMode = true;
     this._binding = ldapInstance;
@@ -26,27 +26,29 @@ class PagedSearchStream extends Readable {
     this._scope = scope;
     this._filter = filter;
     this._pageSize = pageSize;
-    this._searchID = searchID;
+    this._searchId = searchId;
   }
 
   _read() {
     this._binding.pagedSearch(
-      this._base, this._scope, this._filter, this._pageSize, this._searchID,
+      this._base, this._scope, this._filter, this._pageSize, this._searchId,
       (err, page, morePages) => {
         if (err) {
           this.emit('err', err);
-
           this.push(null);
-        } else if (!morePages) {
+        } if (morePages) {
+          console.log('it fails here');
           this.push(page);
-          console.log(
-            'console log after the last page and before the push(null)');
-          this.push(null);
+          console.log('or does it?');
 
         } else {
-          this.push(page);
-          // console.log('else:' + morePages);
+          let flag = this.push(page);
+          console.log(`pushing last page:${flag}`);
+
+          flag = this.push(null);
+          console.log(`pushing null after the last page:${flag}`);
         }
+
       });
   }
 
