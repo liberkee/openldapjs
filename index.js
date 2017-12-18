828f96acb75dbd220ccdcf649ff0d60e7d85af4e
radu <radu@ldap.demoApp.com>
2017-12-18 06:12:34 -0500

    added comments

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const errorList = require('./libs/errors/error_dispenser').errorList;
 /**
  * Library export object
  * @argument LdapClient - Library constructor to be exported and used with new()
- * @argument errorHandler - Custom error dispenser for the library. Wraps ldap errors in custom js error objects.
+ * @argument errorHandler - Custom error dispenser for the library.
+ *  Wraps ldap errors in custom js error objects.
  * @argument errorList - List of all mapped ldap errors
  * 
  */
