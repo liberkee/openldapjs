6fee1cd0ea3d6c48f2db06a966f7c6c1961c7374
Radu <Radu.aribasoiu@huf-group.com>
2018-01-30 13:00:45 +0200

    typo corrected and readme added

---
+++ sample/rename_operation.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const LdapClientLib = require('../index').Client;
+const ldif = require('ldif');
+
+const config = require('./config.json');
+
+const newClient = new LdapClientLib(config.ldapAuthentication.host);
+
+const prePostReadControls = [
+  config.ldapControls.ldapModificationControlPostRead,
+  config.ldapControls.ldapModificationControlPreRead,
+];
+
+newClient.initialize()
+  .then(() => {
+    console.log('Init successfully');
+    return newClient.startTLS(config.ldapAuthentication.pathFileToCert);
+  })
+  .then(() => {
+    console.log('TLS successfully');
+    return newClient.bind(config.ldapAuthentication.dnUser, config.ldapAuthentication.passwordUser);
+  })
+  .then(() => {
+    console.log('Bind successfully');
+    return newClient.rename(config.ldapRename.firstDNChange, config.ldapRename.firstRDN,
+      config.ldapRename.newParent);
+  })
+  .then(() => {
+    console.log('The user was renamed with success');
+    return newClient.rename(config.ldapRename.secondDNChange, config.ldapRename.secondRDN,
+      config.ldapRename.newParent, prePostReadControls);
+  })
+  .then((result) => {
+    const resultJson = ldif.parse(result);
+    const outputOptions = {};
+
+    const JSONstructure = resultJson.toObject(outputOptions);
+    JSONstructure.entries.forEach((element) => {
+      console.log(element);
+    });
+  })
+  .catch((err) => {
+    console.log(err.toString());
+  });
